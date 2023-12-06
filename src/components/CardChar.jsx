8542ccf7fc0e836e4f4891d968b4e3ea4615e54c
Jhonny Cubillos <jonycubi_115@hotmail.com>
2023-12-06 18:31:16 -0500

    componente CardChar

--- src/components/CardChar.jsx
+++ src/components/CardChar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const CardChar = ({ character }) => {
-  const { id, image, name, status } = character;
+  const { id, image, name, status, gender, species } = character;
 
   let life =
     status === "Alive"
@@ -12,16 +12,22 @@ const CardChar = ({ character }) => {
       : "text-[#00D1FF]";
 
   return (
-    <Link to={`/character/:${id}`}>
+    <Link to={`/character/${id}`}>
       <li className="flex flex-col text-center min-h-[15rem] bg-[#43415F] rounded-xl overflow-hidden">
         <div className="w-full">
           <img src={image} alt="" />
         </div>
-        <div className="flex justify-between px-3 py-4 items-center flex-1 gap-2">
-          <span className="text-white w-[60%] font-semibold text-start">
+        <div className="px-3 py-4 items-center flex-1 gap-2">
+          <h3 className=" text-white text-2xl font-black text-start ">
             {name}
-          </span>
-          <span className={`${life} font-semibold`}>{status}</span>
+          </h3>
+
+          <div className="flex justify-between">
+            <p className={` font-semibold text-start text-white`}>
+              {gender} - <span className={`${life}`}> {status} </span>
+            </p>
+            <span className="text-white font-semibold">{species}</span>
+          </div>
         </div>
       </li>
     </Link>
--- src/pages/CharacterScreen.jsx
+++ src/pages/CharacterScreen.jsx
@@ -1,7 +1,78 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 const CharacterScreen = () => {
-  return <div>CharacterScreen</div>;
+  const { id } = useParams();
+  const [character, setCharacter] = useState({});
+
+  console.log(character);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch(
+        `https://rickandmortyapi.com/api/character/${id}`
+      );
+
+      const json = await res.json();
+
+      setCharacter(json);
+    };
+
+    fetchData();
+  }, []);
+
+  const { gender, species, name, image, status, type, origin, location } =
+    character;
+
+  console.log(location);
+
+  let life =
+    status === "Alive"
+      ? "text-[#14FF00]"
+      : status === "Dead"
+      ? "text-[#FF1F1F]"
+      : "text-[#00D1FF]";
+
+  return (
+    <main className=" min-h-screen bg-[#43415f]">
+      <div>
+        <img src={image} alt={name} />
+      </div>
+      <div className="relative px-6 mt-[-1rem] bg-[#43415F]  rounded-t-2xl flex flex-col gap-2">
+        <h3 className="text-center py-6 font-black text-3xl text-white">
+          {name}
+        </h3>
+        <div className="flex justify-between ">
+          <div className="flex gap-2">
+            <span className="font-semibold text-[#b9b7b7]">Gender:</span>
+            <span className="font-bold text-white">{gender}</span>
+          </div>
+          <div className="flex gap-2">
+            <span className="font-semibold text-[#b9b7b7]">Status:</span>
+            <span className={`font-bold ${life}`}>{status}</span>
+          </div>
+        </div>
+        <div className="flex flex-col gap-2">
+          <div className="flex gap-2">
+            <span className="font-semibold text-[#b9b7b7]">Specie:</span>
+            <span className="font-bold text-white">{species}</span>
+          </div>
+          {type && (
+            <div className="flex gap-2">
+              <span className="font-semibold text-[#b9b7b7]">Type:</span>
+              <span className="font-bold text-white"> {type} </span>
+            </div>
+          )}
+        </div>
+        <div className="flex gap-2">
+          <span className="font-semibold text-[#b9b7b7]">
+            Last known location:
+          </span>
+          <span className="font-bold text-white">{}</span>
+        </div>
+      </div>
+    </main>
+  );
 };
 
 export default CharacterScreen;
